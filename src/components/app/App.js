abbd8900fb88ae9f6d86e89b5357ae67f5d0d9a6
shanikoenigsberg <91926757+shanikoenigsberg@users.noreply.github.com>
2022-02-24 19:39:25 -0500

    added navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import { Home } from "../home/home";
 import { Todo } from "../todo/todo.jsx";
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "../header/header"
 
 function App() {
   return (
     <div>
-      {/*<Home />*/}
-      <Todo />
+      
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" index element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
