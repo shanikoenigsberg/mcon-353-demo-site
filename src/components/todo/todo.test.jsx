0c0fe642d01a57da80b8a5cd2641370b7a0c3712
shanikoenigsberg <91926757+shanikoenigsberg@users.noreply.github.com>
2022-03-31 22:48:51 -0400

    final upload!!

--- src/components/todo/todo.test.jsx
+++ src/components/todo/todo.test.jsx
@@ -1,14 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import {Todo} from './todo';
 import {TodoProvider} from "../state/context";
+import userEvent from '@testing-library/user-event';
 
 
 describe('Todo component', () => {
     test("When a task title is typed into the input box and the 'add' button is clicked, a new todo item is added to the list", () => {
         render(<TodoProvider><Todo /></TodoProvider>);
+
         const inputEl = screen.getByPlaceholderText("add task");
-        console.log(inputEl);
+        userEvent.type(inputEl, "mow lawn");
+
+        const buttonEl = screen.getByText("+")
+        userEvent.click();
+
+        const taskEl = screen.getByTestId("todoItem_text");
+        expect(taskEl.textContent).toEqual("mow lawn");
+
+
     });
+
+    test("view todos", () => {
+        render(<Todo></Todo>);
+    })
+
     //test doesnt do anything
     test("All added todo items are shown in a list", () => {
         render(<TodoProvider><Todo /></TodoProvider>);
